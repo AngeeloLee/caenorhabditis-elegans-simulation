ec19371c6eb1c003723156019cfb2730330b8e46
huanghu0 <1525273035@qq.com>
2019-11-27 22:02:42 +0800

    神经元对象

--- code/filehelper.py
+++ code/filehelper.py
@@ -14,7 +14,7 @@ Created on Tue Nov 26 20:34:28 2019
 
 import xlrd
 file = '..\\data\\NeuronConnect.xls'
-def read_excel():
+def read_neuronConnet():
     wb = xlrd.open_workbook(filename=file)##打开文件
     datas  = [[]for i in range(16)]
     for i in range(16):
@@ -30,7 +30,7 @@ def read_excel():
     print(datas)
     return datas
 if __name__ == '__main__':
-    read_excel()
+    read_neuronConnet()
     
 
         
---
+++ code/neuronTypeMap.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 27 20:17:06 2019
+
+@author: asus
+"""
+
+import xlrd
+file = '..\\data\\NeuronType.xls'
+def read_neuronType():
+    wb = xlrd.open_workbook(filename=file)##打开文件
+    sheet = [[]for i in range(12)]
+    neuronType = [[]for i in range(12)]
+    neuron = [[]for i in range(12)]
+    for i in range(12):
+        sheet[i] = wb.sheet_by_index(i)
+    for i in range(12):
+        neuronType[i] = sheet[i].cell_value(0,0)
+    for i in range(12):
+        neuron[i] = sheet[i].col_values(1)
+    
+    d = {}
+    i = 0
+    for row in neuron:
+        for r in row:  
+            d[r] = neuronType[i]
+        i = i+1
+    print(d)
+    return d    
+if __name__ == '__main__':
+    read_neuronType()
+    
