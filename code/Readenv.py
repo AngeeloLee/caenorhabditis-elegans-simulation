b85839eeddd788d20a56f90f51a09178db536076
huanghu0 <1525273035@qq.com>
2019-12-14 20:17:30 +0800

    修改

---
+++ code/Readenv.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 14 19:48:23 2019
+
+@author: asus
+"""
+
+import xlrd
+file = '../data/env.xls'
+def read_env():
+    wb = xlrd.open_workbook(filename=file)##打开文件
+    sheet = wb.sheet_by_index(0)
+    data = []
+    row = sheet.nrows #获取行数
+    for i in range(row):
+        rowData = sheet.row_values(i)#获取第i行的值
+        data.append(rowData)
+    #print(data)
+    return data    
+#if __name__ == '__main__':
+#    read_env()
